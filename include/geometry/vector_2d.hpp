126d1ccdbae016f31c7542551f95fa2e71389c11
jthomperoo <j.thomperoo@hotmail.com>
2022-01-15 13:53:48 +0000

    Add Vector2D methods

--- include/geometry/vector_2d.hpp
+++ include/geometry/vector_2d.hpp
@@ -7,6 +7,7 @@ class Vector2D {
     float x;
     float y;
     Vector2D();
+    Vector2D(const Vector2D &vec);
     explicit Vector2D(float x, float y);
     Vector2D operator+(const Vector2D &other) const;
     void operator+=(const Vector2D &other);
@@ -16,6 +17,19 @@ class Vector2D {
     void operator*=(const Vector2D &other);
     Vector2D operator*(float scaler) const;
     void operator*=(float scaler);
+
+    float Dot(const Vector2D &other) const;
+    float Magnitude() const;
+
+    Vector2D Rotate(const Vector2D &center, float angle) const;
+    Vector2D RotateDeg(const Vector2D &center, float angle) const;
+    Vector2D Invert() const;
+    Vector2D Normalize() const;
+
+    Vector2D* RotateInPlace(const Vector2D &center, float angle);
+    Vector2D* RotateDegInPlace(const Vector2D &center, float angle);
+    Vector2D* InvertInPlace();
+    Vector2D* NormalizeInPlace();
 };
 }; // namespace JamJar
 
